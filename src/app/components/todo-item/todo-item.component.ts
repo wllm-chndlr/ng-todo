3cad66d5cc7bb743cab8ac5d942858068d000b77
William Chandler <chndlr.wllm@gmail.com>
2020-03-23 20:05:57 -0500

    fully functional

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
+import { TodoService } from '../../services/todo.service'
 
 @Component({
   selector: 'app-todo-item',
@@ -8,10 +9,33 @@ import { Todo } from 'src/app/models/Todo';
 })
 export class TodoItemComponent implements OnInit {
     @Input() todo:Todo;
+    @Output() deleteTodo:EventEmitter<Todo> = new EventEmitter();
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit(): void {
   }
 
+  // Set dynamic classes
+  setClasses() {
+      let classes = {
+          todo: true,
+          'is-complete': this.todo.completed
+      }
+
+      return classes;
+  }
+
+  onToggle(todo) {
+    todo.completed = !todo.completed
+    this.todoService.toggleCompleted(todo).subscribe(todo => {
+        console.log(todo);
+    });
+  }
+
+  onDelete(todo) {
+    this.deleteTodo.emit(todo);
+
+
+  }
 }
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { TodoService } from "../../services/todo.service";
 import { Todo } from "../../models/Todo";
 
 @Component({
@@ -9,25 +10,23 @@ import { Todo } from "../../models/Todo";
 export class TodosComponent implements OnInit {
   todos: Todo[];
 
-  constructor() {}
+  constructor(private todoService: TodoService) {}
 
-  ngOnInit(): void {
-    this.todos = [
-      {
-        id: 1,
-        title: "Todo One",
-        completed: false
-      },
-      {
-        id: 2,
-        title: "Todo Two",
-        completed: true
-      },
-      {
-        id: 3,
-        title: "Todo Three",
-        completed: false
-      }
-    ];
+  ngOnInit() {
+    this.todoService.getTodos().subscribe(todos => {
+        this.todos = todos;
+    });
+  }
+
+  deleteTodo(todo:Todo) {
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+
+  addTodo(todo:Todo) {
+    this.todoService.addTodo(todo).subscribe(todo => {
+        this.todos.push(todo);
+    });
+
   }
 }
